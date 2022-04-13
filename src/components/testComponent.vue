0910c6e819e1d2d969f1b379159e9e5e79dcd071
Stepan <kaskader2001@mail.ru>
2022-04-13 21:05:53 +0300

    created & mounted alert test new component for testing

---
+++ src/components/testComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="block">
+      <input v-model="form.text" type="text">
+      {{form.text}}
+      <button @click="getLowerText">Call method</button>
+      {{textLower}}
+      {{toLower}}
+    </div>
+
+    <div class="block">
+      <button @click="show =! show">Show content</button>
+      <p v-if="show">Some content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "testComponent",
+  data() {
+    return {
+      text: ' some text',
+      form: {
+        text: ''
+      },
+      textLower: '',
+
+      show: false
+    }
+  },
+  computed: {
+    toLower () {
+      return this.form.text.toLowerCase()
+    }
+  },
+  methods: {
+    getText () {
+      alert(`Hello ${this.text}`)
+    },
+    getLowerText () {
+      this.textLower = this.form.text.toLowerCase()
+    }
+  },
+  created() {
+    // alert('created hook')
+  },
+  mounted() {
+    // alert('mounted hook')
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.container
+  max-width: 1200px
+  margin: 0 auto
+  display: flex
+  flex-direction: column
+  .block
+    border:  1px solid #cecece
+    border-radius: 10px
+    padding: 10px
+    &:not(:first-child)
+      margin-top: 10px
+</style>
