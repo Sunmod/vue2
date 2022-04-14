7b9e5944650a1c7588b4a8c35634c6cf6f00e1ad
Stepan <kaskader2001@mail.ru>
2022-04-14 10:47:42 +0300

    Start Note App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="app">
-    <div class="container">
-      <div class="block">
-        <input v-model="form.text" type="text">
-        {{form.text}}
-        <button @click="getLowerText">Call method</button>
-        {{textLower}}
-        {{toLower}}
-      </div>
-
-      <div class="block">
-        <button @click="show =! show">Show content</button>
-        <p v-if="show">Some content</p>
-      </div>
+    <h1>{{title}}</h1>
+    <div v-for="(note, index) in notes" :key="index">
+      {{note}}
     </div>
   </div>
 </template>
@@ -21,43 +11,28 @@
 
 export default {
   name: 'App',
-  data() {
+  components: {},
+  data () {
     return {
-      text: ' some text',
-      form: {
-        text: ''
-      },
-      textLower: '',
-
-      show: false
+      title: 'Notes App',
+      notes: [
+        {
+          title: 'First',
+          description: 'some text',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: 'Second',
+          description: 'some text',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: 'Third',
+          description: 'some text',
+          date: new Date(Date.now()).toLocaleString()
+        },
+      ]
     }
-  },
-  computed: {
-    toLower () {
-      return this.form.text.toLowerCase()
-    }
-  },
-  methods: {
-    getText () {
-      alert(`Hello ${this.text}`)
-    },
-    getLowerText () {
-      this.textLower = this.form.text.toLowerCase()
-    }
-  },
+  }
 }
 </script>
-
-<style lang="sass">
-.container
-  max-width: 1200px
-  margin: 0 auto
-  display: flex
-  flex-direction: column
-  .block
-    border:  1px solid #cecece
-    border-radius: 10px
-    padding: 10px
-    &:not(:first-child)
-      margin-top: 10px
-</style>
